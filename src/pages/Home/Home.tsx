5c2a940bcaea651d9d6974a168a1584929e2fe49
harisheincipientinfo <harish.kumar@e.incipientinfo.com>
2021-02-04 08:43:00 +0530

    materialui and bootstrap setup

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import { Grid } from '@material-ui/core';
+
+function Home() {
+  return (
+    <Grid className="App">
+      <h1>Welcome to Dashboard</h1>
+    </Grid>
+  );
+}
+
+export default Home;
